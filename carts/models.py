d7dcaf2b7ea2d8e01283b0c5f487d446d0cb8770
Jianming Chen <loafbaker@hotmail.com>
2026-01-28 08:47:59 +0000

    Modify field definition in CartItem to make it more robust

--- carts/models.py
+++ carts/models.py
@@ -10,7 +10,7 @@ class CartItem(models.Model):
     cart = models.ForeignKey('Cart', null=True, blank=True, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, null=True, blank=True, on_delete=models.CASCADE)
     variations = models.ManyToManyField(Variation, blank=True)
-    quantity = models.IntegerField(default=1)
+    quantity = models.PositiveIntegerField(default=1)
     notes = models.TextField(null=True, blank=True)
     line_total = models.DecimalField(default=Decimal(10.99), max_digits=1000, decimal_places=2)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
